08c6c3c6fa33bcb6771f2fd8945706e9ae5bac55
unknown <nischal.babu@fibonalabs.com>
2022-01-21 10:55:49 +0530

    refactor: 💡 repo url changed to git hub repo

--- src/data/template-repos.ts
+++ src/data/template-repos.ts
@@ -1,6 +1,6 @@
 const TEMPLATE_URLS: Record<string, string> = {
     nestjs_containerized:
-        'https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/be-node-nestjs-server-base-repo',
+        'https://github.com/Fibonalabs-Developers/be-node-nestjs-server-base-repo.git',
     nestjs_serverless:
         'https://git-codecommit.ap-south-1.amazonaws.com/v1/repos/be-node-nestjs-serverless-base-repo',
 }
